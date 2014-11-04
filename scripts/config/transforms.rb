77b7e6512371ccc33e52a01544114c13eeb9c86d
dmronga <dennis.mronga@dfki.de>
2014-11-04 15:23:39 +0100

    Moved deployment to robots/aila/deplyoments --> dynamics branch

--- scripts/config/transforms.rb
+++
@@ -1,8 +0,0 @@
-
-# Test WBC
-
-dynamic_transform "cart_pos_ctrl_right_proxy.output",
-  "setpoint_hand_r" => "base_link"
-
-dynamic_transform "wbc.pose_cart_pos_ctrl_right_arm",
-  "Hand_r" => "base_link"
