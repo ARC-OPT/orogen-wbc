4f26f5f5820f34bb23c4d6ef9bbf8b15752c6e85
dmronga <dennis.mronga@dfki.de>
2014-11-07 17:26:23 +0100

    Merge Tasks Solver, WbcVelocity and RobotModel again --> To much overhead due to port communication

--- tasks/WbcVelocityTask.cpp
+++ tasks/WbcVelocityTask.cpp
@@ -4,6 +4,9 @@
 #include "wbcTypes.hpp"
 #include <base/logging.h>
 #include <wbc/WbcVelocity.hpp>
+#include <kdl_parser/kdl_parser.hpp>
+#include <wbc/HierarchicalWDLSSolver.hpp>
+#include <wbc/RobotModelKDL.hpp>
 
 using namespace wbc;
 using namespace std;
@@ -23,18 +26,76 @@ bool WbcVelocityTask::configureHook(){
     std::vector<wbc::ConstraintConfig> wbc_config = _wbc_config.get();
     std::vector<std::string> joint_names = _joint_names.get();
     joint_weights_ = _initial_joint_weights.get();
-    compute_debug_ = _compute_debug.get();
 
-    // Configure WBC library
+    //
+    // Configure WBC Library
+    //
+
     wbc_ = new WbcVelocity();
     if(!wbc_->configure(wbc_config, joint_names, _task_timeout.get())){
         LOG_ERROR("Unable to configure WBC");
         return false;
     }
-
     LOG_DEBUG("... Configured WBC");
 
-    // Create ports
+    //
+    // Configure Robot model
+    //
+
+    // Load URDF Model:
+    KDL::Tree full_tree, tree;
+    if(!kdl_parser::treeFromFile(_urdf.get(), full_tree))
+    {
+        LOG_ERROR("Unable to parse KDL Tree from URDF file %s", _urdf.get().c_str());
+        return false;
+    }
+
+    //Construct tree, if no reduced tree is given, use full tree
+    std::vector<wbc::SubChainConfig> reduced_tree = _reduced_tree.get();
+    if(reduced_tree.empty())
+        tree = full_tree;
+    else{
+        for(uint i = 0; i < reduced_tree.size(); i++){
+            KDL::Chain chain;
+            if(!full_tree.getChain(reduced_tree[i].root, reduced_tree[i].tip, chain))
+            {
+                LOG_ERROR("Could not extract sub chain between %s and %s from KDL tree", reduced_tree[i].root.c_str(), reduced_tree[i].tip.c_str());
+                return false;
+            }
+            //Root of first subchain will be root of whole KDL tree
+            if(i == 0)
+                tree.addSegment(KDL::Segment(reduced_tree[i].root, KDL::Joint(reduced_tree[i].root,KDL::Joint::None),KDL::Frame::Identity()), "root");
+            tree.addChain(chain, reduced_tree[i].root);
+        }
+    }
+
+    //Create robot model and add task frames
+    robot_model_ = new RobotModelKDL(tree);
+    if(!robot_model_->addTaskFrames(wbc_->getTaskFrameIDs()))
+        return false;
+    LOG_DEBUG("... Configured Robot Model");
+
+
+    //
+    // Configure Solver
+    //
+
+    solver_ = new HierarchicalWDLSSolver();
+    solver_->setNormMax(_norm_max.get());
+    solver_->setSVDMethod(_svd_method.get());
+    solver_->setEpsilon(_epsilon.get());
+
+    std::vector<int> nc_pp = wbc_->getNumberOfConstraintsPP();
+    if(!solver_->configure(nc_pp, wbc_->noOfJoints())){
+        LOG_ERROR("Unable to configure wbc solver");
+        return false;
+    }
+    LOG_DEBUG("... Configured Solver");
+
+    //
+    // Create Ports
+    //
+
     for(uint i = 0; i < wbc_config.size(); i++)
     {
         ConstraintInterface* ci = new ConstraintInterface(wbc_->constraint(wbc_config[i].name));
@@ -49,16 +110,19 @@ bool WbcVelocityTask::configureHook(){
         return false;
     }
 
-    std::vector<uint> nc_pp = wbc_->getNumberOfConstraintsPP();
     equations_.resize(nc_pp.size());
     for(uint prio = 0; prio < nc_pp.size(); prio++)
         equations_[prio].resize(nc_pp[prio], wbc_->noOfJoints());
-    solver_output_.resize(joint_names.size());
+    solver_output_.resize(wbc_->noOfJoints());
     solver_output_.setZero();
-    ctrl_out_.resize(joint_names.size());
+    ctrl_out_.resize(wbc_->noOfJoints());
     ctrl_out_.names = joint_names;
-    robot_vel_.resize(joint_names.size());
+    robot_vel_.resize(wbc_->noOfJoints());
     robot_vel_.setZero();
+    singular_values_.resize(nc_pp.size());
+    inv_condition_numbers_.resize(nc_pp.size());
+    damping_.resize(nc_pp.size());
+    manipulability_.resize(nc_pp.size());
 
     return true;
 }
@@ -79,69 +143,86 @@ void WbcVelocityTask::updateHook(){
 
     base::Time start = base::Time::now();
 
-    if(_task_frames.read(task_frames_) == RTT::NoData){
-        if(state() == RUNNING)
-            state(WAITING_FOR_TASK_FRAMES);
-        return;
-    }
-    if(state() == WAITING_FOR_TASK_FRAMES)
-        state(RUNNING);
-
     for(ConstraintInterfaceMap::iterator it = constraint_interface_map_.begin(); it != constraint_interface_map_.end(); it++)
         it->second->update();
 
     _joint_weights.read(joint_weights_);
 
-    if(_joint_state.read(joint_state_) != RTT::NoData)
-    {
-        for(uint i = 0; i < ctrl_out_.size(); i++){
-            try{
-                robot_vel_(i) = joint_state_.getElementByName(ctrl_out_.names[i]).speed;
-            }
-            catch(std::exception e){
-                LOG_ERROR("WBC expects element %s, but this element is not available in joint state vector", ctrl_out_.names[i].c_str());
-                throw std::invalid_argument("Invalid joint state vector");
-            }
-        }
+    if(_joint_state.read(joint_state_) == RTT::NoData){
+        if(state() != WAITING_FOR_JOINT_STATE)
+            state(WAITING_FOR_JOINT_STATE);
+        return;
     }
 
-    if(_solver_output.read(solver_output_) == RTT::NewData)
-    {
-        if(solver_output_.size() != wbc_->noOfJoints())
-        {
-            LOG_ERROR("Solver output size should be %i but is %i", wbc_->noOfJoints(), solver_output_.size());
-            throw std::invalid_argument("Invalid solver output");
-        }
-        for(uint i = 0; i < ctrl_out_.size(); i++)
-            ctrl_out_[i].speed = solver_output_(i);
-        _ctrl_out.write(ctrl_out_);
-    }
+    if(state() != RUNNING)
+        state(RUNNING);
+
+    //Update Robot Model
+    robot_model_->update(joint_state_);
+    robot_model_->getTFVector(task_frames_);
 
     // Prepare Equation system
     wbc_->prepareEqSystem(task_frames_, equations_);
     wbc_->getConstraintVector(constraints_);
-
     for(uint prio  = 0; prio < equations_.size(); prio++)
-    {
         equations_[prio].W_col = joint_weights_;
 
-        if(compute_debug_)
+    // Solve Equation System
+    solver_->solve(equations_, (Eigen::VectorXd& )solver_output_);
+    for(uint i = 0; i < ctrl_out_.size(); i++)
+        ctrl_out_[i].speed = solver_output_(i);
+
+
+    //Compute debug data
+    for(uint prio = 0; prio < equations_.size(); prio++)
+    {
+        for(uint i = 0; i < constraints_[prio].size(); i++)
         {
-            for(uint i = 0; i < constraints_[prio].size(); i++)
+            for(uint j = 0; j <ctrl_out_.size(); j++)
+                robot_vel_(j) = joint_state_.getElementByName(ctrl_out_.names[j]).speed;
+
+            constraints_[prio][i].y_solution = constraints_[prio][i].A * solver_output_;
+            constraints_[prio][i].y = constraints_[prio][i].A * robot_vel_;
+            constraints_[prio][i].error_y_solution = constraints_[prio][i].y_ref - constraints_[prio][i].y_solution;
+            constraints_[prio][i].error_y = constraints_[prio][i].y_ref - constraints_[prio][i].y;
+        }
+
+        damping_[prio] = solver_->getPriorityData(prio).damping_;
+        singular_values_[prio] = solver_->getPriorityData(prio).singular_values_;
+
+        //Find min and max singular value. Since some singular values might be zero due to deactivated
+        //constraints (zero row weight). Only consider the singular values, which correspond to rows with non-zero weights
+
+        double max_s_val = singular_values_[prio].maxCoeff();
+        double min_s_val = base::infinity<double>();
+        manipulability_[prio] = 1;
+        for(uint i = 0; i < singular_values_[prio].size(); i++)
+        {
+            if(singular_values_[prio](i) < min_s_val && singular_values_[prio](i) > 1e-5)
             {
-                constraints_[prio][i].y_solution = constraints_[prio][i].A * solver_output_;
-                constraints_[prio][i].y = constraints_[prio][i].A * robot_vel_;
-                constraints_[prio][i].error_y_solution = constraints_[prio][i].y_ref - constraints_[prio][i].y_solution;
-                constraints_[prio][i].error_y = constraints_[prio][i].y_ref - constraints_[prio][i].y;
+                min_s_val = singular_values_[prio](i);
+                manipulability_[prio] *= singular_values_[prio](i);
             }
         }
+        //If all row weights are zero, inverse condition number should be 0
+        if(min_s_val == base::infinity<double>())
+            min_s_val = 0;
+        if(manipulability_[prio] == 1)
+            manipulability_[prio] = 0;
+
+        inv_condition_numbers_[prio] = min_s_val / max_s_val;
     }
 
+
     // Write outputs
+    _inv_condition_number_pp.write(inv_condition_numbers_);
+    _damping_pp.write(damping_);
+    _singular_values_pp.write(singular_values_);
+    _manipulability_pp.write(manipulability_);
     _computation_time.write((base::Time::now() - start).toSeconds());
-    _linear_eqn_pp.write(equations_);
-    _constraints.write(constraints_);
     _current_joint_weights.write(joint_weights_);
+    _constraints.write(constraints_);
+    _ctrl_out.write(ctrl_out_);
 }
 
 void WbcVelocityTask::cleanupHook()
@@ -154,4 +235,6 @@ void WbcVelocityTask::cleanupHook()
     }
     constraint_interface_map_.clear();
     delete wbc_;
+    delete solver_;
+    delete robot_model_;
 }
--- tasks/WbcVelocityTask.hpp
+++ tasks/WbcVelocityTask.hpp
@@ -5,28 +5,34 @@
 
 #include "wbc/WbcVelocityTaskBase.hpp"
 #include "ConstraintInterface.hpp"
-
+#include <wbc/TaskFrameKDL.hpp>
 
 namespace wbc {
 
+class HierarchicalWDLSSolver;
+class RobotModelKDL;
 class WbcVelocity;
 
 class WbcVelocityTask : public WbcVelocityTaskBase
 {
     friend class WbcVelocityTaskBase;
 protected:
-    WbcVelocity *wbc_; /** This will create the equation system for the solver*/
 
     ConstraintInterfaceMap constraint_interface_map_; /** Containts port interfaces for each constraint*/
     std::vector<ConstraintsPerPrio> constraints_;   /** Contains all constraints, sorted by priority */
-    std::vector<TaskFrame> task_frames_; /** Task frames coming from the robot model*/
-    std::vector<LinearEqnSystem> equations_; /** Equation system created here, input for the solver*/
+    std::vector<TaskFrameKDL> task_frames_; /** Task frames coming from the robot model*/
+    std::vector<LinearEqnSystem> equations_; /** Equation system, input for the solver*/
     base::VectorXd joint_weights_;
     base::commands::Joints ctrl_out_; /** Control output */
     base::samples::Joints joint_state_; /** Optional joint status input. Is used to compute the constraint status*/
     base::VectorXd solver_output_; /** Solution coming from the solver*/
     base::VectorXd robot_vel_; /** Robot velocity, converted from joint_state*/
-    bool compute_debug_;
+    base::VectorXd damping_, inv_condition_numbers_, manipulability_;
+    std::vector<base::VectorXd> singular_values_;
+
+    WbcVelocity *wbc_; /** This will create the equation system for the solver*/
+    HierarchicalWDLSSolver* solver_;
+    RobotModelKDL *robot_model_;
 
 public:
     WbcVelocityTask(std::string const& name = "wbc::WbcVelocity");
