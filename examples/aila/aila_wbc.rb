efcd56df57e6d69cdcf53bba69ee336da203fa2b
dmronga <dennis.mronga@dfki.de>
2015-09-25 19:11:11 +0200

    Renaming variables

--- examples/aila/aila_wbc.rb
+++ examples/aila/aila_wbc.rb
@@ -31,15 +31,15 @@ Orocos.run "wbc::WbcVelocityTask" => "wbc",
    wbc.port("ctrl_out").connect_to joint_driver.port("command")
 
    # Connect constraints to controllers (Priority 0)
-   wbc.port("ref_cart_pos_ctrl_right_arm").connect_to cart_pos_ctrl_right_arm.port("controlOutput")
+   wbc.port("ref_cart_pos_ctrl_right_arm").connect_to cart_pos_ctrl_right_arm.port("control_output")
    wbc.port("pose_cart_pos_ctrl_right_arm").connect_to cart_pos_ctrl_right_arm.port("feedback")
 
    # Connect constraints to controllers (Priority 1)
-   wbc.port("ref_upper_joint_limits").connect_to upper_limit_avoidance.port("controlOutput")
+   wbc.port("ref_upper_joint_limits").connect_to upper_limit_avoidance.port("control_output")
    joint_driver.port("joint_state").connect_to upper_limit_avoidance.port("feedback")
 
    # Connect constraints to controllers (Priority 2)
-   wbc.port("ref_joint_position_ctrl").connect_to joint_position_ctrl.port("controlOutput")
+   wbc.port("ref_joint_position_ctrl").connect_to joint_position_ctrl.port("control_output")
    joint_driver.port("joint_state").connect_to joint_position_ctrl.port("feedback")
 
    cart_pos_ctrl_right_arm.configure
