21f14b7ad851e0f5ce354d5a34abdbb69b1fc0c7
dmronga <dennis.mronga@dfki.de>
2021-03-03 13:32:49 +0100

    Add task for Scene VelocitySceneQuadraticCost

---
+++ tasks/WbcVelocityQuadraticCostTask.cpp
@@ -0,0 +1,45 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
+
+#include "WbcVelocityQuadraticCostTask.hpp"
+#include <wbc/robot_models/RobotModelKDL.hpp>
+#include <wbc/solvers/qpoases/QPOasesSolver.hpp>
+#include <wbc/scenes/VelocitySceneQuadraticCost.hpp>
+
+using namespace wbc;
+
+WbcVelocityQuadraticCostTask::WbcVelocityQuadraticCostTask(std::string const& name)
+    : WbcVelocityQuadraticCostTaskBase(name){
+    robot_model = std::make_shared<RobotModelKDL>();
+    solver = std::make_shared<QPOASESSolver>();
+    wbc_scene = std::make_shared<VelocitySceneQuadraticCost>(robot_model, solver);
+}
+
+WbcVelocityQuadraticCostTask::~WbcVelocityQuadraticCostTask(){
+}
+
+bool WbcVelocityQuadraticCostTask::configureHook(){
+    if (! WbcVelocityQuadraticCostTaskBase::configureHook())
+        return false;
+
+    std::dynamic_pointer_cast<QPOASESSolver>(solver)->setMaxNoWSR(_n_wsr.get());
+    std::dynamic_pointer_cast<QPOASESSolver>(solver)->setOptions(_solver_options.get());
+
+    return true;
+}
+bool WbcVelocityQuadraticCostTask::startHook(){
+    if (! WbcVelocityQuadraticCostTaskBase::startHook())
+        return false;
+    return true;
+}
+void WbcVelocityQuadraticCostTask::updateHook(){
+    WbcVelocityQuadraticCostTaskBase::updateHook();
+}
+void WbcVelocityQuadraticCostTask::errorHook(){
+    WbcVelocityQuadraticCostTaskBase::errorHook();
+}
+void WbcVelocityQuadraticCostTask::stopHook(){
+    WbcVelocityQuadraticCostTaskBase::stopHook();
+}
+void WbcVelocityQuadraticCostTask::cleanupHook(){
+    WbcVelocityQuadraticCostTaskBase::cleanupHook();
+}
